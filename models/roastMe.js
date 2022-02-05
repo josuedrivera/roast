e45822cd3dcba8fd57aa312ba019389da1e6f2c7
Josue <josue.bills@gmail.com>
2022-02-04 23:49:26 -0800

    add model.

---
+++ models/roastMe.js
@@ -0,0 +1,11 @@
+// campground.js in course
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const RoastMeSchema = new Schema ({
+    name: String,
+    title: String,
+    location: String,
+});
+
+module.exports = mongoose.model('RoastMe', RoastMeSchema);
